489b627ff79043108b3a26d88c1cc0084775a386
bipinkatuwal567 <bipinkatuwal098@gmail.com>
2025-05-17 16:55:07 +0545

    Add mongodb config file

---
+++ backend/src/config/mongodb.config.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("MongoDB connection failed", error.message);
+    process.exit(1); //Stop the server if DB fails
+  }
+};
+
+export default connectDB;
